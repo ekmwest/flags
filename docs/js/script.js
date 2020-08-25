1a3d1832ab0192f60b3944338f2c887f7f49af5c
Karl West <mickel.west@gmail.com>
2020-08-25 22:27:12 +0200

    adjusted open behaviour

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,14 +1,18 @@
 document.addEventListener('click', event => {
-    let openImages = document.querySelectorAll('.open');
-
-    for (let i = 0; i < openImages.length; i++) {
-        if (openImages[i] === event.target) {
-            continue;
-        }
-        openImages[i].classList.remove('open');
+    let open = document.querySelector('.open');
+    if (open) {
+        open.classList.remove('open');
+        return;
     }
 
-    if (event.target.matches('img')) {
-        event.target.classList.toggle('open');
+    if (!event.target.matches('img')) {
+        return;
     }
+
+    let imgContainer = event.target.closest('.image-container');
+    imgContainer.classList.add('open');
+    // console.log(imgContainer);
+
+
+
 })
