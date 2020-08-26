49ced264f49d389cff4a0e5c1441b98a430f6a26
Karl West <mickel.west@gmail.com>
2020-08-26 22:06:22 +0200

    testing modal transitions

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,18 +1,58 @@
 document.addEventListener('click', event => {
-    let open = document.querySelector('.open');
-    if (open) {
-        open.classList.remove('open');
+    if (!event.target.matches('.flag')) {
         return;
     }
 
-    if (!event.target.matches('img')) {
+    openModal(event.target);
+
+    // let open = document.querySelector('.open');
+    // if (open) {
+    //     open.classList.remove('open');
+    //     return;
+    // }
+
+    // if (!event.target.matches('.flag')) {
+    //     return;
+    // }
+
+    // let imgContainer = event.target.closest('.flag-container');
+    // imgContainer.classList.add('open');
+
+});
+
+document.addEventListener('click', event => {
+    const modal = event.target.closest('.modal');
+
+    if (!modal) {
         return;
     }
 
-    let imgContainer = event.target.closest('.image-container');
-    imgContainer.classList.add('open');
-    // console.log(imgContainer);
+    closeModal();
+});
 
+function openModal(flagElement) {
+    const modal = document.createElement('div');
+    modal.classList.add('modal');
 
+    const img = document.createElement('img');
+    img.onload = event => {
+        setTimeout(() => modal.classList.add('open'), 1);
+    }
+    img.classList.add('flag-in-modal');
+    img.src = flagElement.src;
+    modal.appendChild(img);
 
-})
+    document.body.appendChild(modal);
+
+    // setTimeout(closeModal, 3000);
+}
+
+function closeModal() {
+    const modal = document.querySelector('.modal');
+
+    if (!modal) {
+        return;
+    }
+
+    document.body.removeChild(modal);
+}
