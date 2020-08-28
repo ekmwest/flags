15f9aeda9f5891ec8039bb3a457ee15fc6422246
Karl West <mickel.west@gmail.com>
2020-08-28 22:50:55 +0200

    added flags

---
+++ build.js
@@ -0,0 +1,50 @@
+import * as path from "https://deno.land/std@0.66.0/path/mod.ts";
+import { exists } from "https://deno.land/std@0.66.0/fs/mod.ts";
+import { sideBuild } from "https://cdn.jsdelivr.net/gh/ekmwest/side@1.0.4/mod.js";
+import { DB } from "https://deno.land/x/sqlite@v2.3.0/mod.ts";
+
+const rootPath = Deno.cwd();
+const sourcePath = path.join(rootPath, 'src');
+const targetPath = path.join(rootPath, 'docs');
+const componentsPath = path.join(sourcePath, 'components');
+const svgPath = path.join(rootPath, 'data/svg');
+
+copySvgs();
+await buildFlagsComponents();
+sideBuild(sourcePath, targetPath, componentsPath, false);
+
+async function copySvgs() {
+    for await (const dirEntry of Deno.readDir(svgPath)) {
+        const targetFilePath = path.join(targetPath, dirEntry.name);
+        const targetExists = await exists(targetFilePath);
+
+        if (targetExists) {
+            continue;
+        }
+
+        const sourceFilePath = path.join(svgPath, dirEntry.name);
+
+        Deno.copyFile(sourceFilePath, targetFilePath);
+    }
+}
+
+async function buildFlagsComponents() {
+    const db = new DB('data/countries.db');
+
+    const htmlElements = [];
+
+    for (const [code, name] of db.query("SELECT code, name FROM countries ORDER BY name")) {
+
+        htmlElements.push(`
+        <div>
+            <div class="flag-container">
+                <img class="flag" src="/${code}.svg" alt="${name}" />
+                <span class="country-name">${name}</span>
+            </div>
+        </div>`);
+    }
+
+    await Deno.writeTextFile(path.join(componentsPath, "flags.html"), htmlElements.join(''));
+
+    db.close();
+}
---
+++ src/js/script.js
@@ -0,0 +1,100 @@
+document.addEventListener('click', event => {
+    if (!event.target.matches('.flag')) {
+        return;
+    }
+
+    openModal(event.target);
+});
+
+document.addEventListener('click', event => {
+    const modal = event.target.closest('.modal');
+
+    if (!modal) {
+        return;
+    }
+
+    closeModal();
+});
+
+document.addEventListener('keyup', event => {
+    if (event.key !== 'Escape') {
+        return;
+    }
+
+    const modal = document.querySelector('.modal');
+    if (!modal) {
+        return;
+    }
+
+    closeModal();
+})
+
+function openModal(flagElement) {
+    const modal = document.createElement('div');
+    modal.classList.add('modal');
+
+    const img = document.createElement('img');
+    img.classList.add('flag-in-modal');
+    modal.appendChild(img);
+
+    var rect = flagElement.getBoundingClientRect();
+
+    img.style.top = rect.top + 'px';
+    img.style.left = rect.left + 'px';
+    img.style.width = rect.width + 'px';
+    img.style.height = rect.height + 'px';
+
+    img.dataset.top = img.style.top;
+    img.dataset.left = img.style.left;
+    img.dataset.width = img.style.width;
+    img.dataset.height = img.style.height;
+
+    const viewportAspectRatio = document.documentElement.clientWidth / document.documentElement.clientHeight;
+    const flagAspectRatio = rect.width / rect.height;
+
+    img.onload = event => {
+        setTimeout(() => {
+            modal.classList.add('open');
+
+            let zoom = 0.9;
+
+            if (flagAspectRatio > viewportAspectRatio) {
+                zoom = zoom * document.documentElement.clientWidth / rect.width;
+            } else {
+                zoom = zoom * document.documentElement.clientHeight / rect.height;
+            }
+
+            const zoomedWidth = zoom * rect.width;
+            const zoomedHeight = zoom * rect.height;
+
+            img.style.width = zoomedWidth + 'px';
+            img.style.height = zoomedHeight + 'px';
+
+            img.style.left = ((document.documentElement.clientWidth - zoomedWidth) / 2) + 'px';
+            img.style.top = ((document.documentElement.clientHeight - zoomedHeight) / 2) + 'px';
+        }, 10);
+    }
+
+    img.src = flagElement.src;
+
+    document.body.appendChild(modal);
+}
+
+function closeModal() {
+    const modal = document.querySelector('.modal');
+
+    if (!modal) {
+        return;
+    }
+
+    const img = modal.querySelector('.flag-in-modal');
+
+    img.style.top = img.dataset.top;
+    img.style.left = img.dataset.left;
+    img.style.width = img.dataset.width;
+    img.style.height = img.dataset.height;
+
+    modal.classList.remove('open');
+
+    setTimeout(() => document.body.removeChild(modal), 500);
+}
