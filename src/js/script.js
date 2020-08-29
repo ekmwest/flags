6b16b56771aa3c258b3fef43695f92d261a82f13
Karl West <mickel.west@gmail.com>
2020-08-29 18:07:23 +0200

    transition using transform

--- src/js/script.js
+++ src/js/script.js
@@ -37,56 +37,54 @@ function openModal(flagElement) {
     img.classList.add('flag-in-modal');
     modal.appendChild(img);
 
-    const caption = document.createElement('p');
-    caption.classList.add('modal-caption');
-    caption.textContent = flagElement.getAttribute('alt');
-    modal.appendChild(caption);
-
     var rect = flagElement.getBoundingClientRect();
-
-    img.style.top = rect.top + 'px';
-    img.style.left = rect.left + 'px';
-    img.style.width = rect.width + 'px';
-    img.style.height = rect.height + 'px';
-
-    img.dataset.top = img.style.top;
-    img.dataset.left = img.style.left;
-    img.dataset.width = img.style.width;
-    img.dataset.height = img.style.height;
 
     const viewportAspectRatio = document.documentElement.clientWidth / document.documentElement.clientHeight;
     const flagAspectRatio = rect.width / rect.height;
 
+    let zoom = 0.9;
+
+    if (flagAspectRatio > viewportAspectRatio) {
+        zoom = zoom * document.documentElement.clientWidth / rect.width;
+    } else {
+        zoom = zoom * document.documentElement.clientHeight / rect.height;
+        zoom = 0.9 * zoom; // extra space for caption
+    }
+
+    const zoomedWidth = zoom * rect.width;
+    const zoomedHeight = zoom * rect.height;
+
+    const zoomedLeft = ((document.documentElement.clientWidth - zoomedWidth) / 2);
+    const zoomedTop = ((document.documentElement.clientHeight - zoomedHeight) / 2);
+
+    img.style.width = zoomedWidth + 'px';
+    img.style.height = zoomedHeight + 'px';
+
+    img.style.left = zoomedLeft + 'px';
+    img.style.top = zoomedTop + 'px';
+
+    const scale = 1 / zoom;
+
+    const translateX = (rect.width / 2 + rect.left - zoomedWidth / 2 - zoomedLeft);
+    const translateY = (rect.height / 2 + rect.top - zoomedHeight / 2 - zoomedTop);
+
+    img.style.transform = `translate(${translateX}px, ${translateY}px) scale(${scale})`;
+    img.dataset.transform = img.style.transform;
+
+    const caption = document.createElement('p');
+    caption.classList.add('modal-caption');
+    caption.textContent = flagElement.getAttribute('alt');
+    caption.style.top = (zoomedHeight + zoomedTop) + 'px';
+    caption.style.left = zoomedLeft + 'px';
+    caption.style.width = zoomedWidth + 'px';
+    modal.appendChild(caption);
+
     img.onload = event => {
         setTimeout(() => {
             modal.classList.add('open');
-
-            let zoom = 0.9;
-
-            if (flagAspectRatio > viewportAspectRatio) {
-                zoom = zoom * document.documentElement.clientWidth / rect.width;
-            } else {
-                zoom = zoom * document.documentElement.clientHeight / rect.height;
-                zoom = 0.9 * zoom; // extra space for caption
-            }
-
-            const zoomedWidth = zoom * rect.width;
-            const zoomedHeight = zoom * rect.height;
-
-            const zoomedLeft = ((document.documentElement.clientWidth - zoomedWidth) / 2);
-            const zoomedTop = ((document.documentElement.clientHeight - zoomedHeight) / 2);
-
-            img.style.width = zoomedWidth + 'px';
-            img.style.height = zoomedHeight + 'px';
-
-            img.style.left = zoomedLeft + 'px';
-            img.style.top = zoomedTop + 'px';
-
-            caption.style.top = (zoomedHeight + zoomedTop) + 'px';
-            caption.style.left = zoomedLeft + 'px';
-            caption.style.width = zoomedWidth + 'px';
+            img.style.transform = "";
         }, 10);
-    }
+    };
 
     img.src = flagElement.src;
 
@@ -101,13 +99,9 @@ function closeModal() {
     }
 
     const img = modal.querySelector('.flag-in-modal');
-
-    img.style.top = img.dataset.top;
-    img.style.left = img.dataset.left;
-    img.style.width = img.dataset.width;
-    img.style.height = img.dataset.height;
+    img.style.transform = img.dataset.transform;
 
     modal.classList.remove('open');
 
-    setTimeout(() => document.body.removeChild(modal), 500);
+    setTimeout(() => document.body.removeChild(modal), 300);
 }
